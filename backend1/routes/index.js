f11a4c9a2ed0779f3c98374f8996a3563879d98a
savi-1311 <shambhavishandilya01@gmail.com>
2020-03-20 22:10:19 +0530

    Menu Ready

--- backend1/routes/index.js
+++ backend1/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).send('home-page'));
+router.get('/', (req, res) => res.status(200).render('home1'));
 router.get('/dashboard1', (req, res) => {
     if (req.session.user1)
       res.status(200).send(req.session.user1)
@@ -10,7 +10,11 @@ router.get('/dashboard1', (req, res) => {
   });
   router.get('/dashboard2', (req, res) => {
     if (req.session.user2)
-      res.status(200).send(req.session.user2)
+      {
+        res.render('dashboard2', {
+          user2: req.session.user2,
+        })
+      }
     else
       res.status(401).send('login for this');
   });
